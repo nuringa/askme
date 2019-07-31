67d71390c1a35d5a91b75a1eec19c798a43c64d9
admin.karima@gmail.com <admin.karima@gmail.com>
2019-07-31 15:45:40 +0300

    jquery fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
-//= require activestorage
 //= require_tree .
 
-$(function(){
-    $('#ask-button').click(function(){
+$(function () {
+    $('#ask-button').click(function () {
         $('#ask-form').slideToggle(300);
         return false;
     });
