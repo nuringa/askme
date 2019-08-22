e796c2c0495f38099156f00dd7a0d242d580941f
admin.karima@gmail.com <admin.karima@gmail.com>
2019-08-22 12:50:33 +0300

    hashtags model assosiations and controller with show

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
+  has_many :question_tags
+  has_many :tags, through: :question_tags
 
   validates :text, presence: true, length: { maximum: 255 }
   validates :user, :text, presence: true
