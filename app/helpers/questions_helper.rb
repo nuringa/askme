054156c008c9730b9c8bb6c89d4bf1bbe5b87a5a
admin.karima@gmail.com <admin.karima@gmail.com>
2019-08-22 16:24:01 +0300

    fixed update and delete of questions tags and helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,5 @@
 module QuestionsHelper
+  def question_tags(string)
+    string.downcase.split(' ').select { |word| word.start_with?('#') }
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,10 @@
 class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
-  has_many :question_tags
+  has_many :question_tags, dependent: :destroy
   has_many :tags, through: :question_tags
 
-  after_commit :create_update_tags
+  after_save :create_update_tags
 
   validates :text, presence: true, length: { maximum: 255 }
   validates :user, :text, presence: true
