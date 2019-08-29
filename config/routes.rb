b29fb49150e8087f32bb3e231bae5baf2507cfde
admin.karima@gmail.com <admin.karima@gmail.com>
2019-08-29 12:46:28 +0300

    session resource fix and routes for links

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,8 @@ Rails.application.routes.draw do
   get 'tags/show'
   root to: 'users#index'
 
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
-
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
   resources :tags, only: [:show], param: :name
 end
